078ef477f6c299e03e978beabf5b50e4f225f6af
Nabute <nabute925@gmail.com>
2020-03-07 18:24:20 +0300

    first commit

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## [0.0.1] - 2020-7-3
+
+* First Time Development.
---
+++ lib/ethiopiancalendar.dart
@@ -0,0 +1,7 @@
+library ethiopiancalendar;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
