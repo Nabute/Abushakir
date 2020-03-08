a60bc16ff1f064fb5a69b9fd978c01291e2871ac
Nabute <nabute925@gmail.com>
2020-03-08 03:09:29 +0300

    second commit

--- lib/ethiopiancalendar.dart
+++ lib/ethiopiancalendar.dart
@@ -1,7 +1,10 @@
 library ethiopiancalendar;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+///// A Calculator.
+//class Calculator {
+//  /// Returns [value] plus 1.
+//  int addOne(int value) => value + 1;
+//}
+
+//export "ethiopian:calendar" show EthiopianCalendar;
+//from 'src/ethiopian_calendar.dart' import 'EthiopianCalendar';
