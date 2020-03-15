f8d19d5a7380337d4ccd07b19bacfbfc9a0c660e
icnahom <60504285+icnahom@users.noreply.github.com>
2020-03-15 14:22:39 +0300

    Update ethiopiancalendar_test.dart

--- test/ethiopiancalendar_test.dart
+++ test/ethiopiancalendar_test.dart
@@ -3,11 +3,22 @@
 import 'package:ethiopiancalendar/ethiopiancalendar.dart';
 
 void main() {
-//  test('adds one to input values', () {
-//    final calculator = Calculator();
-//    expect(calculator.addOne(2), 3);
-//    expect(calculator.addOne(-7), -6);
-//    expect(calculator.addOne(0), 1);
-//    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
-//  });
+
+  // All the starting dates of the months
+  //TODO መስከረም - Regular = September 11, Leap Year = September 12 
+  //TODO ጥቅምት - Regular = October 11, Leap Year = October 12 
+  //TODO ኅዳር - Regular = November 10, Leap Year = November 11 
+  //TODO ታኅሳስ - Regular = December 10, Leap Year = December 12 
+  //TODO ጥር - Regular = January 09, Leap Year = September 10 
+  //TODO የካቲት - Regular = February 08, Leap Year = September 09 
+  //TODO መጋቢት - Regular = March 10 
+  //TODO ሚያዝያ - Regular = April 09 
+  //TODO ግንቦት - Regular = May 09 
+  //TODO ሰኔ - Regular = June 08 
+  //TODO ኃምሌ - Regular = July 08 
+  //TODO ነሐሴ - Regular = August 07
+  //TODO ጷጉሜን - Regular = September 06 
+
+  // Holidays
+
 }
