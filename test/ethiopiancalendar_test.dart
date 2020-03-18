075cdd9ddaa088b9c955679fc83f7d664b381b90
icnahom <60504285+icnahom@users.noreply.github.com>
2020-03-18 12:49:09 +0300

    test

--- test/ethiopiancalendar_test.dart
+++ test/ethiopiancalendar_test.dart
@@ -8,12 +8,6 @@ void main() {
   // Parameterized Constructors
 
 
-  //FIXME: Day is reduced by 1
-  test('Should Print the parsed Date and Time', () {
-    final ec = EthiopianCalendar.parse("2012-07-07 18:26:31.449");
-    expect(ec.toString(), "2012-07-07 18:26:31.449");
-  });
-
   test('Testing Year on Parameterized Constructor', (){
     final ec = EthiopianCalendar(year: 2012, month: 07, day: 07);
     expect(ec.year, 2012);
@@ -37,8 +31,13 @@ void main() {
   });
 
 
-  // Named Constructors
+  // Named Constructors (.parse)
   
+  //FIXME: Day is reduced by 1
+  test('Should Print the parsed Date and Time', () {
+    final ec = EthiopianCalendar.parse("2012-07-07 18:26:31.449");
+    expect(ec.toString(), "2012-07-07 18:26:31.449");
+  });
 
   test('Testing Year on Named Constructor', (){
     final ec = EthiopianCalendar.parse("2012-07-07 18:26:31.449");
@@ -83,4 +82,129 @@ void main() {
     final ec = EthiopianCalendar.parse("2012-07-07 18:26:31.449");
     expect(ec.millisecond, 449);
   });
+
+
+
+  // Named Constructors (.now)
+  
+
+  test('Testing Year on .now() Named Constructor', (){
+    final ec = EthiopianCalendar.now();
+    expect(ec.year, 2012);
+  });
+
+  test('Testing Month on .now() Named Constructor', (){
+    final ec = EthiopianCalendar.now();
+    expect(ec.month, 07);
+  });
+  
+  test('Testing Month Geez on .now() Named Constructor', (){
+    final ec = EthiopianCalendar.now();
+    expect(ec.monthGeez, "መጋቢት");
+  });
+
+  test('Testing Day on .now() Named Constructor', (){
+    final ec = EthiopianCalendar.now();
+    expect(ec.day, 10);
+  });
+
+  test('Testing Day Geez on .now() Named Constructor', (){
+    final ec = EthiopianCalendar.now();
+    expect(ec.dayGeez, "፲");
+  });
+
+  test('Testing Hour on .now() Named Constructor', (){
+    final ec = EthiopianCalendar.now();
+    expect(ec.hour, 6);
+  });
+
+  test('Testing Minute on .now() Named Constructor', (){
+    final ec = EthiopianCalendar.now();
+    expect(ec.minute, DateTime.now().minute);
+
+  });
+
+  test('Testing Second on .now() Named Constructor', (){
+    final ec = EthiopianCalendar.now();
+    expect(ec.second, DateTime.now().second);
+  });
+
+
+  // BahireHasab
+
+
+  test('Testing Abekte', (){
+    final bh = BahireHasab();
+    expect(bh.getAbekte(), 6);
+  });
+
+  test('Testing Metkih', (){
+    final bh = BahireHasab();
+    expect(bh.getMetkih(), 24);
+  });
+
+  test('Testing Nenewe', (){
+    final bh = BahireHasab();
+    expect(bh.getNenewe(), {'month': 'የካቲት', 'date': 2});
+  });
+
+
+  // getSingleBealOrTsom
+
+
+  test("Testing 'ነነዌ' on getSingleBealOrTsom", (){
+    final bh = BahireHasab();
+    expect(bh.getSingleBealOrTsom("ነነዌ"), {'month': 'የካቲት', 'date': 2});
+  });
+  test("Testing 'ዓቢይ ጾም' on getSingleBealOrTsom", (){
+    final bh = BahireHasab();
+    expect(bh.getSingleBealOrTsom("ዓቢይ ጾም"), {'month': 'የካቲት', 'date': 16});
+  });
+
+  test("Testing 'ደብረ ዘይት' on getSingleBealOrTsom", (){
+    final bh = BahireHasab();
+    expect(bh.getSingleBealOrTsom("ደብረ ዘይት"), {'month': 'መጋቢት', 'date': 13});
+  });
+
+  test("Testing 'ሆሣዕና' on getSingleBealOrTsom", (){
+    final bh = BahireHasab();
+    expect(bh.getSingleBealOrTsom("ሆሣዕና"), {'month': 'ሚያዝያ', 'date': 4});
+  });
+
+  test("Testing 'ስቅለት' on getSingleBealOrTsom", (){
+    final bh = BahireHasab();
+    expect(bh.getSingleBealOrTsom("ስቅለት"), {'month': 'ሚያዝያ', 'date': 9});
+  });
+
+  test("Testing 'ትንሳኤ' on getSingleBealOrTsom", (){
+    final bh = BahireHasab();
+    expect(bh.getSingleBealOrTsom("ትንሳኤ"), {'month': 'ሚያዝያ', 'date': 11});
+  });
+
+  test("Testing 'ርክበ ካህናት' on getSingleBealOrTsom", (){
+    final bh = BahireHasab();
+    expect(bh.getSingleBealOrTsom("ርክበ ካህናት"), {'month': 'ግንቦት', 'date': 11});
+  });
+
+  test("Testing 'ዕርገት' on getSingleBealOrTsom", (){
+    final bh = BahireHasab();
+    expect(bh.getSingleBealOrTsom("ዕርገት"), {'month': 'ግንቦት', 'date': 20});
+  });
+
+  test("Testing 'ጰራቅሊጦስ' on getSingleBealOrTsom", (){
+    final bh = BahireHasab();
+    expect(bh.getSingleBealOrTsom("ጰራቅሊጦስ"), {'month': 'ግንቦት', 'date': 30});
+  });
+
+  test("Testing 'ጾመ ሐዋርያት' on getSingleBealOrTsom", (){
+    final bh = BahireHasab();
+    expect(bh.getSingleBealOrTsom("ጾመ ሐዋርያት"), {'month': 'ሰኔ', 'date': 1});
+  });
+
+  test("Testing 'ጾመ ድህነት' on getSingleBealOrTsom", (){
+    final bh = BahireHasab();
+    expect(bh.getSingleBealOrTsom("ጾመ ድህነት"), {'month': 'ሰኔ', 'date': 3});
+  });
+
+ 
 }
